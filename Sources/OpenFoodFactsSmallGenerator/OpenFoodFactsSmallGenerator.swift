d20287ac1b80f64c6a7ee0e95fccb34a59737d25
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2023-02-08 21:50:48 +0100

    Remove ProductDownloader

--- Sources/OpenFoodFactsSmallGenerator/OpenFoodFactsSmallGenerator.swift
+++ Sources/OpenFoodFactsSmallGenerator/OpenFoodFactsSmallGenerator.swift
@@ -2,9 +2,7 @@ import Foundation
 @main
 public struct OpenFoodFactsSmallGenerator {
     public static func main() async throws{
-		let downloader = ProductDownloader(session: .shared, language: .german, downloadDirectory: FileManager.default.temporaryDirectory)
 		do {
-//			try await downloader.start()
 			try await ProductExtractor().start(source: .init(fileURLWithPath: "openfoodfacts-products.jsonl"), target: .init(fileURLWithPath: "./public"))
 		} catch {
 			print("Download failed with error: \(error)")
