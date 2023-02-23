0a9a5c963e115432bf1153dde7b835eb5cf35ada
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2023-02-23 13:19:23 +0100

    Update OpenFoodFactsSmallGenerator.swift

--- Sources/SmallProductsGenerator/OpenFoodFactsSmallGenerator.swift
+++ Sources/SmallProductsGenerator/OpenFoodFactsSmallGenerator.swift
@@ -7,25 +7,17 @@ public struct OpenFoodFactsSmallGenerator {
     public static func main() async throws{
 		let target = URL(fileURLWithPath: "./public")
 		var packages = [LanguagePackage]()
-		do {
-			let nutritionPrivacyExtractor = ProductExtractor<NutritionPrivacyModels.ProductPreview>()
-			let nutritionPrivacyPackages = try await nutritionPrivacyExtractor.start(source: .init(fileURLWithPath: "nutritionprivacy-products.jsonl"),
+		let nutritionPrivacyExtractor = ProductExtractor<NutritionPrivacyModels.ProductPreview>()
+		let nutritionPrivacyPackages = try await nutritionPrivacyExtractor.start(source: .init(fileURLWithPath: "nutritionprivacy-products.jsonl"),
 																				target: target,
 																				datasource: .nutritionPrivacy)
-			let openFoodFactsExtractor = ProductExtractor<OpenFoodFactsModels.Product>()
-			let openFoodFactsPackages = try await openFoodFactsExtractor.start(source: .init(fileURLWithPath: "openfoodfacts-products.jsonl"),
-																				target: target,
-																			   datasource: .openFoodFacts)
-			packages.append(contentsOf: nutritionPrivacyPackages)
-			packages.append(contentsOf: openFoodFactsPackages)
-		} catch {
-			print("Download failed with error: \(error)")
-		}
-		do {
-			let data = try JSONEncoder().encode(packages)
-			try data.write(to: target.appendingPathComponent("overview.json"))
-		} catch {
-			print("Overview creation failed with error: \(error)")
-		}
+		let openFoodFactsExtractor = ProductExtractor<OpenFoodFactsModels.Product>()
+		let openFoodFactsPackages = try await openFoodFactsExtractor.start(source: .init(fileURLWithPath: "openfoodfacts-products.jsonl"),
+																				target: target,	
+																				 datasource: .openFoodFacts)
+		packages.append(contentsOf: nutritionPrivacyPackages)
+		packages.append(contentsOf: openFoodFactsPackages)
+		let data = try JSONEncoder().encode(packages)
+		try data.write(to: target.appendingPathComponent("overview.json"))
     }
 }
