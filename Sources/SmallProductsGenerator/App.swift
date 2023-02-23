b7973c435e123b600965bd4ef54c3f5a4d3b19c7
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2023-02-23 22:02:20 +0100

    Rename to app and update style

--- Sources/SmallProductsGenerator/App.swift
+++ Sources/SmallProductsGenerator/App.swift
@@ -3,21 +3,21 @@ import OpenFoodFactsModels
 import NutritionPrivacyModels
 
 @main
-public struct OpenFoodFactsSmallGenerator {
-    public static func main() async throws{
+public struct App {
+	public static func main() async throws{
 		let target = URL(fileURLWithPath: "./public")
 		var packages = [LanguagePackage]()
 		let nutritionPrivacyExtractor = ProductExtractor<NutritionPrivacyModels.ProductPreview>()
 		let nutritionPrivacyPackages = try await nutritionPrivacyExtractor.start(source: .init(fileURLWithPath: "nutritionprivacy-products.jsonl"),
-																				target: target,
-																				datasource: .nutritionPrivacy)
+																				 target: target,
+																				 datasource: .nutritionPrivacy)
 		let openFoodFactsExtractor = ProductExtractor<OpenFoodFactsModels.Product>()
 		let openFoodFactsPackages = try await openFoodFactsExtractor.start(source: .init(fileURLWithPath: "openfoodfacts-products.jsonl"),
-																				target: target,	
-																				 datasource: .openFoodFacts)
+																		   target: target,
+																		   datasource: .openFoodFacts)
 		packages.append(contentsOf: nutritionPrivacyPackages)
 		packages.append(contentsOf: openFoodFactsPackages)
 		let data = try JSONEncoder().encode(packages)
 		try data.write(to: target.appendingPathComponent("overview.json"))
-    }
+	}
 }
